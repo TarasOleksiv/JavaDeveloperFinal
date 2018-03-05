e69c5f03d7d711331aef3f95192cb8422a1f2d5b
TarasOleksiv <toleksiv69@gmail.com>
2018-03-05 16:27:05 +0200

    remove some sql scripts

--- src/main/resources/database/initDB_3.sql
+++
@@ -1,6 +0,0 @@
-# 12. Create view to calculate user salary.
-CREATE OR REPLACE VIEW user_salary AS
-  SELECT e.id event_id, e.date, e.duration, et.salary_coef, u.id user_id, u.username, u.hourly_rate FROM events e
-    LEFT JOIN event_types et on e.event_type = et.id
-    LEFT JOIN event_users eu on e.id = eu.event_id
-    LEFT JOIN users u on eu.user_id = u.id;
